786f1b4ea2512c4b9973b1c99e7435f861bcfd5a
BrycensRanch <brycengranville@outlook.com>
2023-04-18 10:36:56 -0400

    YOU ARE NOT FAST ENOUGH.... YOU ARE NOT BRAVE ENOUGH

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,14 +13,26 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://brycens.tech',
-  integrations: [preact(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), fonts({
-    url: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
-  }), mdx(), sitemap(), partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  }), tailwind(), robotsTxt(), purgecss(), critters(), compressor()],
+	site: 'https://brycens.tech',
+	integrations: [
+		preact(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+		fonts({
+			url: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
+		}),
+		mdx(),
+		sitemap(),
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+		tailwind(),
+		robotsTxt(),
+		purgecss(),
+		critters(),
+		compressor(),
+	],
 });
