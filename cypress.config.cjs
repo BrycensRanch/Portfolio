a3a90b2f8c833adb33f579c4334766b5dfac3a04
Brycen G <brycengranville@outlook.com>
2023-04-19 13:06:48 +0000

    chore(repo): renovate dev enviroment

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,17 +1,10 @@
 const { defineConfig } = require('cypress');
-const codeCoverageTask = require('@cypress/code-coverage/task');
 
 module.exports = defineConfig({
-	env: {
-		codeCoverage: {
-			url: '/coverage',
-			exclude: 'cypress/**/*.*',
-		},
-	},
+	env: {},
 	e2e: {
 		baseUrl: 'http://localhost:3000',
 		setupNodeEvents(on, config) {
-			codeCoverageTask(on, config);
 			// include any other plugin code...
 
 			// It's IMPORTANT to return the config object
--- cypress/plugins/index.js
+++
@@ -1,6 +0,0 @@
-const useBabelRC = require('@cypress/code-coverage/use-babelrc');
-
-module.exports = (on, config) => {
-	// Used to instrument code ran like unit tests
-	on('file:preprocessor', useBabelRC);
-};
